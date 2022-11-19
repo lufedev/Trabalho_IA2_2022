002f47e990a0a81540224c100eb1a02de7a24756
Luiz <felipeluizsantos.k@gmail.com>
2022-11-19 09:12:16 -0300

    Arquivos finais

--- dataframe.py
+++ dataframe.py
@@ -3,7 +3,7 @@ from pandas_profiling import ProfileReport
 from scipy.stats import entropy
 import numpy as np
 
-df = pd.read_csv('small_grocery.csv')
+df = pd.read_csv("Trabalho_IA\data\large_grocery.csv",delimiter=";")
 
 profile = ProfileReport(df, title="Pandas Profiling Report", explorative=True)
-profile.to_file("your_report.html")
+profile.to_file("large_report.html")
